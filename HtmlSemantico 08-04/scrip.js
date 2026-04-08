ceacff9b502649087868d6c45423a955a999ac2d
Robson656 <suairma1598@gmail.com>
2026-04-08 17:24:49 -0300

    Add files via upload

---
+++ HtmlSemantico 08-04/scrip.js
@@ -0,0 +1,27 @@
+function alterarNome() {
+    let trocarNome = document.getElementById("inputNome").value;
+
+    if (trocarNome === "") {
+        alert("Digite um nome!")
+    } else{
+        document.getElementById("nomeUsuario").innerHTML = trocarNome;
+    }
+}
+function alterarCurso(){
+    let trocarCurso = document.getElementById("inputCurso").value;
+    if (trocarCurso === ""){
+        alert("Coloque um curso!")
+    }
+    else {
+        document.getElementById("nomeCurso").innerHTML = trocarCurso
+    }
+}
+function alterarStatus(){
+    let trocarStatus = document.getElementById("inputStatus").value;
+    if(trocarStatus === ""){
+        alert("indique um Status")
+    }
+    else {
+        document.getElementById("nomeStatus").innerHTML = trocarStatus
+    }
+}
